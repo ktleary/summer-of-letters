d9c057bbe5b7094770312df18f7ab8f4c5ef27b5
ktleary <kevin@ktleary.com>
2021-01-17 14:30:21 +0800

    clean up unused vars. todo:update tests

--- src/components/tab-quick.js
+++ src/components/tab-quick.js
@@ -1,21 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import { nanoid } from "nanoid";
 import ResultsRow from "./result-row";
 import ResultsHeader from "./results-header";
-import Row from "./row";
-import { Cell } from "./cell";
 import SearchBox from "./search-box";
-
-const labels = ["Eng", "ER", "Red", "RR"];
-const id = nanoid();
 
 const TabQuickWrapper = styled.div`
   padding-top: 0;
-`;
-const ResultsHeaderRow = styled(Row)`
-  font-size: 16px;
-  margin-top: 16px;
 `;
 
 const QuickResultsRow = styled(ResultsRow)`
@@ -23,19 +13,6 @@ const QuickResultsRow = styled(ResultsRow)`
   font-size: 16px;
   margin-top: 16px;
 `;
-
-const ResultsCell = styled(Cell)`
-  color: rgba(255, 255, 255, 0.89);
-  font-size: 16px;
-`;
-
-// const ResultsHeader = () => (
-//   <ResultsHeaderRow>
-//     {labels.map((label, i) => (
-//       <ResultsCell key={`label-${id}-${i}`}>{label}</ResultsCell>
-//     ))}
-//   </ResultsHeaderRow>
-// );
 
 const TabQuick = ({ setQuickQuery, query, resultData }) => {
   const handleChange = (e) => {
